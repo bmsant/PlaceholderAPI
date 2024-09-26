018ac7943b5513831d161235c08da3a1fa285700
Bruno Santos <bdevelop@outlook.fr>
2024-09-27 00:20:50 +0200

    Endpoint with Query Params

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET') {
     try {
+      const parsedUrl = url.parse(req.url, true);
+      const searchParams = parsedUrl.query;
+      const firstName = searchParams.firstname || "Guest";
+      const lastName = searchParams.lastname || "Tseug";
+
+      const fullName = `${firstName} ${lastName}`;
+
       res.writeHead(200, {
         'Content-Type': 'text/plain',
         'Cache-Control': 'no-cache',
       });
-      res.end("Hello World!");
+      res.end(`Hello ${fullName}`);
     } catch (error) {
       console.error('Error:', error);
       res.writeHead(500, { 'Content-Type': 'application/json' });
